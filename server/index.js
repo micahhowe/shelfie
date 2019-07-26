04b9f718c2d5e2893b29cf0b2bb9c93e8e926da1
micahhowe <micahhowe182@gmail.com>
2019-07-26 10:01:09 -0600

    added some components

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive')
+const app = express()
 const ctrl = require('./controller')
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance);
+}).catch(err => console.log(err))
+
+app.use(express.json())
