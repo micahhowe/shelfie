9f559c193f4e6b6401a66c6226d423a7f9f29948
micahhowe <micahhowe182@gmail.com>
2019-07-26 11:10:57 -0600

    i got the cancel button to work with sheer brute force

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -12,7 +12,7 @@ export default class Form extends Component {
         }
     handleImgChange(e) {
         this.setState({ imgUrl: e.target.value })
-        //console.log(e.target.value)
+        console.log(e.target.value)
     }
     handleNameChange(e) {
         this.setState({ product: e.target.value })
@@ -22,15 +22,23 @@ export default class Form extends Component {
         this.setState({ price: e.target.value })
         console.log(e.target.value)
     }
+    resetText() {
+        let text = document.getElementById('text')
+        let text1 = document.getElementById('text1')
+        let text2 = document.getElementById('text2')
+        text.value = ''
+        text1.value = ''
+        text2.value = ''
+    }
     
     render (){
         
         return (
         <div className="form">
-                         <input onChange={e => this.handleImgChange(e)} type='text' placeholder="IMG" />
-                         <input onChange={e => this.handleNameChange(e)} type='text' placeholder="Product Name"/>
-                         <input onChange={e => this.handlePriceChange(e)} type='text' placeholder="Price"/>
-                         <button>Cancel</button>
+                         <input id="text" onChange={e => this.handleImgChange(e)} type='text' placeholder="IMG" />
+                         <input id="text1" onChange={e => this.handleNameChange(e)} type='text' placeholder="Product Name"/>
+                         <input id="text2" onChange={e => this.handlePriceChange(e)} type='text' placeholder="Price"/>
+                         <button onClick={() => this.resetText()}>Cancel</button>
                          <button>Add to Inventory</button>
         </div>
       )
